bcfc9060748f7b73d60384c4310667bc770d7bc5
Abhav Goel <goelabhav2002@gmail.com>
2024-07-29 18:42:40 +0530

    User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,34 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String , 
+        required:true,
+
+    },
+
+    email : {
+        type: String,
+        required : true,
+        unique : true
+    },
+
+    password : {
+        type:String, 
+        required : true
+    },
+    group : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Group'
+    }
+
+});
+
+userSchema.static("matchPassword", async (enteredPassword) => {
+    return await bcrypt.compare(enteredPassword, this.password);
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
